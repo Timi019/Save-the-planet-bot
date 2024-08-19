981038883c000a1ce09f647b118328d1ec0de1c5
Tymek <tymon.sidzina@icloud.com>
2024-08-19 12:15:22 +0200

    modified:   README.md 	fresh advice:   main.py

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 This bot will help you with saving the environment by sending some advice to you
 
 - code from my other repo: [Discord-bot](https://github.com/Timi019/Discord-bot)
-
+- advice from https://oceanservice.noaa.gov/ocean/earthday.html
 ---
 
 todo list:
 - ~~fix the sheduler~~
 - add more commands
-- add more advice
-- add more specific advice
+- ~~add more advice~~
+- ~~add more specific advice~~
--- main.py
+++ main.py
@@ -8,17 +8,25 @@ intents = discord.Intents.default()
 intents.message_content = True
 token = os.environ["TOKEN"]
 bot = commands.Bot(command_prefix='/',intents=intents)
+earthot = bot.get_emoji(1275024848405532735)
 channel_id = 1275019583446843433
 facts = [
-    "Segreguj śmieci",
-    "Oszczędzaj wodę",
-    "Wybieraj odnawialne źródła energii"
+    "Ograniczaj, wykorzystuj ponownie i poddawaj recyklingowi. Ograniczaj to, co wyrzucasz. Postępuj zgodnie z zasadą trzech „R”, aby oszczędzać zasoby naturalne i miejsce na wysypiskach.",
+    "Zostań wolontariuszem. Zgłoś się jako wolontariusz w akcjach sprzątania w swojej okolicy. Możesz również zaangażować się w ochronę pobliskich zbiorników wodnych.",
+    "Edukuj. Kiedy rozwiniesz swoją wiedzę o ratowaniu planety, możesz pomóc innym zrozumieć znaczenie i wartość naszych zasobów naturalnych.",
+    "Oszczędzaj wodę. Im mniej wody zużywasz, tym mniej spływu i ścieków ostatecznie trafia do oceanu.",
+    "Kupuj rozważnie. Kupuj mniej plastiku i noś wielorazową torbę na zakupy.",
+    "Używaj żarówek o długiej żywotności. Energooszczędne żarówki redukują emisję gazów cieplarnianych. Pamiętaj o wyłączeniu światłą, gdy wychodzisz z pokoju!",
+    "Posadź drzewo. Drzewa dostarczają pożywienia i tlenu. Pomagają oszczędzać energię, oczyszczać powietrze i walczyć ze zmianą klimatu.",
+    "Nie wlewaj chemikaliów do wody. Staraj się wybierać nietoksyczne chemikalia.",
+    "Rower jest bardziej eko od pojazdów spalinowych. Jeździj nim więcej, aby walczyć ze zmianami klimatu.",
+    "Jeśli to możliwe, używaj elektrycznych środków transportu lub komunikacji miejskiej."
 ]
 
 async def send_hourly_message():
     advice = random.choice(facts)
     c = bot.get_channel(channel_id)
-    await c.send(f"Oto godzinna wskazówka: {advice}")
+    await c.send(f"Oto godzinna wskazówka: {advice}. Dbaj o planetę aby nie była <:earthot:1275024848405532735>")
 
 @bot.event
 async def on_ready():
@@ -29,7 +37,7 @@ async def on_ready():
         #initializing scheduler
         scheduler = AsyncIOScheduler()
 
-        #sends "s!t" to the channel when time hits 10/20/30/40/50/60 seconds, like 12:04:20 PM
+        #sends advice to the channel 
         scheduler.add_job(send_hourly_message, 'interval', minutes=1) 
 
         #starting the scheduler
@@ -37,14 +45,19 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-@bot.tree.command(name='czesc')
+@bot.tree.command(name='witaj')
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Cześć, {interaction.user.mention}!")
 
 @bot.tree.command(name='gadaj')
 @app_commands.describe(tekstdogadania = "Co mam powiedzieć?")
 async def say(interaction: discord.Interaction, tekstdogadania: str):
-    await interaction.response.send_message(f"{interaction.user.name} powiedział: {tekstdogadania}")
+    await interaction.response.send_message(f"{interaction.user.mention} powiedział: {tekstdogadania}")
+
+@bot.tree.command(name='pomocy')
+async def help(interaction: discord.Interaction):
+    advice = random.choice(facts)
+    await interaction.response.send_message(f"Oto twoja wskazówka: {advice}. Dbaj o planetę aby nie była <:earthot:1275024848405532735>")
 
 @bot.tree.command(name='pa')
 async def bye(interaction: discord.Interaction):
