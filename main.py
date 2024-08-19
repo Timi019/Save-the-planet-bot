67be8f14b9e56f93e0e86b56c65b339989e8ac3a
Tymek <tymon.sidzina@icloud.com>
2024-08-19 11:16:21 +0200

    added working sheduler

--- main.py
+++ main.py
@@ -8,22 +8,32 @@ intents = discord.Intents.default()
 intents.message_content = True
 token = os.environ["TOKEN"]
 bot = commands.Bot(command_prefix='/',intents=intents)
+channel_id = 1275019583446843433
 facts = [
     "Segreguj śmieci",
     "Oszczędzaj wodę",
     "Wybieraj odnawialne źródła energii"
 ]
 
+async def send_hourly_message():
+    advice = random.choice(facts)
+    c = bot.get_channel(channel_id)
+    await c.send(f"Oto godzinna wskazówka: {advice}")
+
 @bot.event
 async def on_ready():
-    # Set up the hourly task 
-    scheduler = AsyncIOScheduler() 
-    scheduler.add_job(send_hourly_message, 'interval')
-    scheduler.start() 
-    try:
+    try:    
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} command(s)")
         print('bot is ready')
+        #initializing scheduler
+        scheduler = AsyncIOScheduler()
+
+        #sends "s!t" to the channel when time hits 10/20/30/40/50/60 seconds, like 12:04:20 PM
+        scheduler.add_job(send_hourly_message, 'interval', minutes=1) 
+
+        #starting the scheduler
+        scheduler.start()
     except Exception as e:
         print(e)
 
@@ -35,10 +45,6 @@ async def hello(interaction: discord.Interaction):
 @app_commands.describe(tekstdogadania = "Co mam powiedzieć?")
 async def say(interaction: discord.Interaction, tekstdogadania: str):
     await interaction.response.send_message(f"{interaction.user.name} powiedział: {tekstdogadania}")
-
-async def send_hourly_message(interaction: discord.Interaction):
-    advice = random.choice(facts)
-    await interaction.response.send_message(f"Oto godzinna wskazówka: {advice}")
 
 @bot.tree.command(name='pa')
 async def bye(interaction: discord.Interaction):
